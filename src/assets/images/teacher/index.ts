a77d09925acb9d146ba8d78733a3582293993685
sounava24 <sounavachatterjee32@gmail.com>
2024-08-28 00:54:02 +0530

    Our Team, Past Requireters, Alumni, Contact Info, Footer

---
+++ src/assets/images/teacher/index.ts
@@ -0,0 +1,17 @@
+import group1 from './DRSOURABHKRDAS.png'
+import group2 from './/PROFMRINALKANTIKUMAR.png'
+
+export{group1,group2};
+export const ts = [
+    {
+        name: "DR. SOURABH KR. DAS",
+        image: group1,
+        linkedin: "",
+        designation: "Principal, KGEC"
+    },
+    {
+        name: "PROF. MRINAL KANTI KUMAR",
+        image: group2,
+        linkedin: "https://www.linkedin.com/in/mrinal-kanti-kumar-3b125838/",
+        designation: "Training & Placement Officer, KGEC"
+    }]
