0a65fb68fb09ff400bb9bcc2d4561188e73938a7
Md Kaif Sardar <mkaifsard564773@gmail.com>
2024-08-29 03:55:34 +0530

    changed some stuffs

--- src/data/fromTNP.tsx
+++ src/data/fromTNP.tsx
@@ -1,38 +1,44 @@
 export const faculties = [
-    {
-      "name": "Faculty Name",
-      "designation": "Designation",
-      "image": "/images/logo1.png",
-      "description": "Lorem ipsum dolor amet, consectetur adipiscing elit. Augue lacus egestas himenaeos justo sed consequat lacus nec. Accumsan sem ex curae malesuada cubilia dictum vel aenean. Integer tempus accumsan tincidunt, sem consectetur taciti. Morbi nostra."
-    },
-    {
-      "name": "Faculty Name",
-      "designation": "Designation",
-      "image": "/images/logo1.png",
-      "description": "Lorem ipsum dolor amet, consectetur adipiscing elit. Augue lacus egestas himenaeos justo sed consequat lacus nec. Accumsan sem ex curae malesuada cubilia dictum vel aenean. Integer tempus accumsan tincidunt, sem consectetur taciti. Morbi nostra."
-    },
-    {
-      "name": "Faculty Name",
-      "designation": "Designation",
-      "image": "/images/logo1.png",
-      "description": "Lorem ipsum dolor amet, consectetur adipiscing elit. Augue lacus egestas himenaeos justo sed consequat lacus nec. Accumsan sem ex curae malesuada cubilia dictum vel aenean. Integer tempus accumsan tincidunt, sem consectetur taciti. Morbi nostra."
-    },
-    {
-      "name": "Faculty Name",
-      "designation": "Designation",
-      "image": "/images/logo1.png",
-      "description": "Lorem ipsum dolor amet, consectetur adipiscing elit. Augue lacus egestas himenaeos justo sed consequat lacus nec. Accumsan sem ex curae malesuada cubilia dictum vel aenean. Integer tempus accumsan tincidunt, sem consectetur taciti. Morbi nostra."
-    },
-    {
-      "name": "Faculty Name",
-      "designation": "Designation",
-      "image": "/images/logo1.png",
-      "description": "Lorem ipsum dolor amet, consectetur adipiscing elit. Augue lacus egestas himenaeos justo sed consequat lacus nec. Accumsan sem ex curae malesuada cubilia dictum vel aenean. Integer tempus accumsan tincidunt, sem consectetur taciti. Morbi nostra."
-    },
-    {
-      "name": "Faculty Name",
-      "designation": "Designation",
-      "image": "/images/logo1.png",
-      "description": "Lorem ipsum dolor amet, consectetur adipiscing elit. Augue lacus egestas himenaeos justo sed consequat lacus nec. Accumsan sem ex curae malesuada cubilia dictum vel aenean. Integer tempus accumsan tincidunt, sem consectetur taciti. Morbi nostra."
-    }
-]
+  {
+    name: 'Faculty Name',
+    designation: 'Designation',
+    image: '/images/logo1.png',
+    description:
+      'Lorem ipsum dolor amet, consectetur adipiscing elit. Augue lacus egestas himenaeos justo sed consequat lacus nec. Accumsan sem ex curae malesuada cubilia dictum vel aenean. Integer tempus accumsan tincidunt, sem consectetur taciti. Morbi nostra.',
+  },
+  {
+    name: 'Faculty Name',
+    designation: 'Designation',
+    image: '/images/logo1.png',
+    description:
+      'Lorem ipsum dolor amet, consectetur adipiscing elit. Augue lacus egestas himenaeos justo sed consequat lacus nec. Accumsan sem ex curae malesuada cubilia dictum vel aenean. Integer tempus accumsan tincidunt, sem consectetur taciti. Morbi nostra.',
+  },
+  {
+    name: 'Faculty Name',
+    designation: 'Designation',
+    image: '/images/logo1.png',
+    description:
+      'Lorem ipsum dolor amet, consectetur adipiscing elit. Augue lacus egestas himenaeos justo sed consequat lacus nec. Accumsan sem ex curae malesuada cubilia dictum vel aenean. Integer tempus accumsan tincidunt, sem consectetur taciti. Morbi nostra.',
+  },
+  {
+    name: 'Faculty Name',
+    designation: 'Designation',
+    image: '/images/logo1.png',
+    description:
+      'Lorem ipsum dolor amet, consectetur adipiscing elit. Augue lacus egestas himenaeos justo sed consequat lacus nec. Accumsan sem ex curae malesuada cubilia dictum vel aenean. Integer tempus accumsan tincidunt, sem consectetur taciti. Morbi nostra.',
+  },
+  {
+    name: 'Faculty Name',
+    designation: 'Designation',
+    image: '/images/logo1.png',
+    description:
+      'Lorem ipsum dolor amet, consectetur adipiscing elit. Augue lacus egestas himenaeos justo sed consequat lacus nec. Accumsan sem ex curae malesuada cubilia dictum vel aenean. Integer tempus accumsan tincidunt, sem consectetur taciti. Morbi nostra.',
+  },
+  {
+    name: 'Faculty Name',
+    designation: 'Designation',
+    image: '/images/logo1.png',
+    description:
+      'Lorem ipsum dolor amet, consectetur adipiscing elit. Augue lacus egestas himenaeos justo sed consequat lacus nec. Accumsan sem ex curae malesuada cubilia dictum vel aenean. Integer tempus accumsan tincidunt, sem consectetur taciti. Morbi nostra.',
+  },
+];
