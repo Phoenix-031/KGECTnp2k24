84f0d02033e0716dbd8da895e286b6b0290874bd
NiteshCodes7 <workwithnites@gmail.com>
2024-08-29 23:18:11 +0530

    changed scroll bar css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,26 +89,6 @@ const config = {
     },
   },
   plugins: [
-    function ({ addUtilities }) {
-      addUtilities({
-        '.custom-scrollbar': {
-          '&::-webkit-scrollbar': {
-            width: '12px',
-          },
-          '&::-webkit-scrollbar-track': {
-            background: '#f1f1f1',
-          },
-          '&::-webkit-scrollbar-thumb': {
-            backgroundColor: '#0C84AA', 
-            borderRadius: '20px',
-            border: '3px solid #f1f1f1',
-          },
-          '&::-webkit-scrollbar-thumb:hover': {
-            backgroundColor: '#006494', 
-          },
-        },
-      });
-    },
   ],
 } satisfies Config;
 
