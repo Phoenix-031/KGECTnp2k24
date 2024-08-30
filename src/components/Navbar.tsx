eb0577a28652093157f7d47f8bd430dbfb195771
Akash Biswas <132830479+akashbiswas7074@users.noreply.github.com>
2024-08-30 07:49:27 +0530

    new

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -76,75 +76,116 @@ export const Navbar = () => {
 
   return (
     <>
-    <div className='fixed z-50'>
-      <nav
-        ref={navbarRef}
-        className={`bg-black/50 inset-0 backdrop-blur-md w-screen text-white font-semibold p-4 text-sm md:flex md:justify-between  top-0 md:items-center transition-transform duration-300 z-50 ${
-          isVisible ? 'translate-y-0' : '-translate-y-full'
-        }`}
-      >
-        <div className='container flex justify-between items-center'>
-          <a href='#' className='text-2xl font-bold ml-[-14px]'>
-            <Image alt='Navbar Logo' src={'/kgec.svg'} width={40} height={40} />
-          </a>
-          <div className=''>
-          <div className={`${getMenuClasses()} pl-4 gap-5`}>
-            <Link href='/' className='hover:text-gray-300 text-xs lg:text-base' onClick={handleLinkClick}>
-              HOME
-            </Link>
-            <Link href='#whykgec' className='hover:text-gray-300 text-xs lg:text-base ' onClick={handleLinkClick}>
-              WHY RECRUIT US
-            </Link>
-            <Link href='#tnp' className='hover:text-gray-300  text-xs lg:text-base' onClick={handleLinkClick}>
-              FROM T&P CELL
-            </Link>
-            <Link href='#placement-statistics' className='hover:text-gray-300 text-xs lg:text-base' onClick={handleLinkClick}>
-              PLACEMENT STATISTICS
-            </Link>
-            <Link href='#past-recruiters' className='hover:text-gray-300 text-xs lg:text-base' onClick={handleLinkClick}>
-              PAST RECRUITERS
-            </Link>
-            <Link href='#contacts' className='hover:text-gray-300 text-md  lg:text-base' onClick={handleLinkClick}>
-              CONTACT US
-            </Link>
-            <Link href='/signup' className='hover:text-gray-300 text-sm lg:text-base' onClick={handleLinkClick}>
-              STUDENT LOGIN
-            </Link>
+      <div className='fixed z-50'>
+        <nav
+          ref={navbarRef}
+          className={`bg-black/50 inset-0 backdrop-blur-md w-screen text-white font-semibold p-4 text-sm md:flex md:justify-between  top-0 md:items-center transition-transform duration-300 z-50 ${
+            isVisible ? 'translate-y-0' : '-translate-y-full'
+          }`}
+        >
+          <div className='container flex justify-between items-center'>
+            <a
+              href='#'
+              className='text-2xl font-bold ml-[-14px]'
+            >
+              <Image
+                alt='Navbar Logo'
+                src={'/kgec.svg'}
+                width={40}
+                height={40}
+              />
+            </a>
+            <div className=''>
+              <div className={`${getMenuClasses()} pl-4 gap-5`}>
+                <Link
+                  href='/'
+                  className='hover:text-gray-300 text-xs lg:text-base'
+                  onClick={handleLinkClick}
+                >
+                  HOME
+                </Link>
+                <Link
+                  href='#whykgec'
+                  className='hover:text-gray-300 text-xs lg:text-base '
+                  onClick={handleLinkClick}
+                >
+                  WHY RECRUIT US
+                </Link>
+                <Link
+                  href='#tnp'
+                  className='hover:text-gray-300  text-xs lg:text-base'
+                  onClick={handleLinkClick}
+                >
+                  FROM T&P CELL
+                </Link>
+                <Link
+                  href='#placement-statistics'
+                  className='hover:text-gray-300 text-xs lg:text-base'
+                  onClick={handleLinkClick}
+                >
+                  PLACEMENT STATISTICS
+                </Link>
+                <Link
+                  href='#past-recruiters'
+                  className='hover:text-gray-300 text-xs lg:text-base'
+                  onClick={handleLinkClick}
+                >
+                  PAST RECRUITERS
+                </Link>
+                <Link
+                  href='#contacts'
+                  className='hover:text-gray-300 text-md  lg:text-base'
+                  onClick={handleLinkClick}
+                >
+                  CONTACT US
+                </Link>
+                <Link
+                  href='/signup'
+                  className='hover:text-gray-300 text-sm lg:text-base'
+                  onClick={handleLinkClick}
+                >
+                  STUDENT LOGIN
+                </Link>
+              </div>
+              <div className='flex items-center mr-[-14px] lg:hidden'>
+                <button onClick={() => setIsOpen(!isOpen)}>
+                  <svg
+                    className={`transition-transform duration-300 ${isOpen ? 'rotate-90 scale-down' : 'rotate-neg-90 scale-up'}`}
+                    width='24'
+                    height='24'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                    style={{ cursor: 'pointer' }}
+                  >
+                    <g id='Icon/Outline/menu'>
+                      {isOpen ? (
+                        <path
+                          fillRule='evenodd'
+                          clipRule='evenodd'
+                          d='M18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289Z'
+                          fill='#ffffff'
+                        />
+                      ) : (
+                        <path
+                          fillRule='evenodd'
+                          clipRule='evenodd'
+                          d='M4 6C4 5.44772 4.44772 5 5 5H19C19.5523 5 20 5.44772 20 6C20 6.55228 19.5523 7 19 7H5C4.44772 7 4 6.55228 4 6ZM5 11C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11H5ZM5 17C4.44772 17 4 17.4477 4 18C4 18.5523 4.44772 19 5 19H19C19.5523 19 20 18.5523 20 18C20 17.4477 19.5523 17 19 17H5Z'
+                          fill='#ffffff'
+                        />
+                      )}
+                    </g>
+                  </svg>
+                </button>
+              </div>
+            </div>
           </div>
-          <div className='flex items-center mr-[-14px] lg:hidden'>
-            <button onClick={() => setIsOpen(!isOpen)}>
-              <svg
-                className={`transition-transform duration-300 ${isOpen ? 'rotate-90 scale-down' : 'rotate-neg-90 scale-up'}`}
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-                style={{ cursor: 'pointer' }}
-              >
-                <g id='Icon/Outline/menu'>
-                  {isOpen ? (
-                    <path
-                      fillRule='evenodd'
-                      clipRule='evenodd'
-                      d='M18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289Z'
-                      fill='#ffffff'
-                    />
-                  ) : (
-                    <path
-                      fillRule='evenodd'
-                      clipRule='evenodd'
-                      d='M4 6C4 5.44772 4.44772 5 5 5H19C19.5523 5 20 5.44772 20 6C20 6.55228 19.5523 7 19 7H5C4.44772 7 4 6.55228 4 6ZM5 11C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11H5ZM5 17C4.44772 17 4 17.4477 4 18C4 18.5523 4.44772 19 5 19H19C19.5523 19 20 18.5523 20 18C20 17.4477 19.5523 17 19 17H5Z'
-                      fill='#ffffff'
-                    />
-                  )}
-                </g>
-              </svg>
-            </button>
-          </div></div>
-        </div>
-      </nav></div>
-      <div className={getOverlayClasses()} onClick={() => setIsOpen(false)}></div>
+        </nav>
+      </div>
+      <div
+        className={getOverlayClasses()}
+        onClick={() => setIsOpen(false)}
+      ></div>
     </>
   );
 };
